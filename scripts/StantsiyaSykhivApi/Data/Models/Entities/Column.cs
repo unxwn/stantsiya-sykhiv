f7258f87bdae046445c1f0bff1c14bb1e38ec07d
unxwn <46842875+unxwn@users.noreply.github.com>
2025-05-29 17:37:34 +0000

    Deploying to gh-pages from @ unxwn/stantsiya-sykhiv@9e25076648df64cbd646f77c1dc4dc3ed83f1515 🚀

---
+++ scripts/StantsiyaSykhivApi/Data/Models/Entities/Column.cs
@@ -0,0 +1,13 @@
+﻿namespace StantsiyaSykhivApi.Data.Models.Entities
+{
+    public class Column
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public int Order { get; set; }
+        public int BoardId { get; set; }
+        public Board Board { get; set; } = null!;
+
+        public ICollection<TaskColumnLink>? TaskColumnLinks { get; set; }
+    }
+}
